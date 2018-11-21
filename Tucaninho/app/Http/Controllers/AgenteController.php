4dedfcafb6fb7618437a9d9ca4d7145ec9fd20e4
Lucca Carvalho <luccalafonte97@gmail.com>
2018-11-21 16:28:30 -0200

    Implementação da exclusão da conta de cliente e agente

--- Tucaninho/app/Http/Controllers/AgenteController.php
+++ Tucaninho/app/Http/Controllers/AgenteController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Agente;
 
 class AgenteController extends Controller
 {
     public function logout(){
         Auth::guard('agente')->logout();
         return redirect('/');
+    }
+
+    public function deletar($encrypted_email){
+        $email = decrypt($encrypted_email);
+
+        $agente = Agente::where('email_agente', $email)->first();
+        if($agente===null) return redirect()->action('PedidosController@listaPedidosAgente');
+        Auth::guard('agente')->logout();
+        $agente->delete();
+        return view('home');
     }
 }
